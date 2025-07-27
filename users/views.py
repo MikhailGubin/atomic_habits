704cf730a5aa48fadebe3ecf16e836ef0960b01e
Mikhail Gubin <mgubin82@gmail.com>
2025-07-27 23:24:27 +0300

    Small changes in views.py

--- users/views.py
+++ users/views.py
@@ -95,8 +95,8 @@ class CustomTokenRefreshView(TokenRefreshView):
             401: "Неверное имя пользователя или пароль",
         },
     )
-    def post(self, request, args, kwargs):
-        return super().post(request, args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
