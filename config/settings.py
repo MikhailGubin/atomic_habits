cfaf4e235743c360e68851a863e24ae4af1317f3
Mikhail Gubin <mgubin82@gmail.com>
2025-08-16 15:11:07 +0300

    Fixed in settings.py. Added jobs lint in ci.yml.

--- config/settings.py
+++ config/settings.py
@@ -107,7 +107,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = []
-STATIC_ROOT = BASE_DIR/ 'staticfiles/'
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
